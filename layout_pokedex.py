3ecfb90064a86141242e618533d20d09dd19ef46
gabs <gabriellima.glgs@gmail.com>
2019-10-13 10:25:23 -0300

    label colorida

--- layout_pokedex.py
+++ layout_pokedex.py
@@ -6,6 +6,7 @@ root = Tk()
 class Application:
     def __init__(self,master=None):
         layout_superior = Label(root, text = '------- Pokedex -------')
+        layout_superior.configure(bg = '#87CEFA')
         layout_superior.pack(side = TOP, expand = False, fill = 'x')
 
         listagem_pokemon = Listbox() #div usada para alocar a lista dos nomes e id dos pokemons
@@ -17,16 +18,16 @@ class Application:
         div_1.configure(command = listagem_pokemon.yview) #Ligado a div de listagem de pokemon
         listagem_pokemon.configure(yscrollcommand = div_1.set) #sentido da rolagem
 
-        div_2 = Canvas(bg = "black")
-        div_2.pack()
-        div_2['width'] = 300
-        div_2['height'] = 150
-        img = PhotoImage(file = "mapa.png")
-        div_2.create_image(0,0, image = img ,anchor = NW)
-
         for id_pokemon, pokemon in enumerate(pokemon_lista): #laço para gerar ordenar os nome e id dos pokemon
-            texto = f'#{id_pokemon + 1:0>3}  -  {pokemon["Nome"]}'
-            listagem_pokemon.insert(END,texto)
+            texto = f'#{id_pokemon + 1:0>3}  -  {pokemon["Nome"]}' #variavel onde é armazenado o id e o nome dos pokemon pelo laço
+            listagem_pokemon.insert(END,texto) #metodo que insere esses dados na div
+
+        div_2 = Canvas(bg = "black") #div onde será inserido as imagens
+        div_2.pack() 
+        div_2['width'] = 300 #largura 
+        div_2['height'] = 150 # altura
+        img = PhotoImage(file = "mapa.png") 
+        div_2.create_image(0,0, image = img ,anchor = NW)
 
 root.geometry("360x520+10+10")
 Application(root)
