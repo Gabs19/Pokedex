aafb2398885483ee656cc49afc7d2db9d5a9bd9e
gabs <gabriellima.glgs@gmail.com>
2019-10-14 11:32:46 -0300

    captura de dados

--- layout_pokedex.py
+++ layout_pokedex.py
@@ -3,24 +3,47 @@ from pokedex import pokemon_lista
 
 root = Tk()
 
+
 class Application:
+    
     def __init__(self,master=None):
         layout_superior = Label(root, text = '------- Pokedex -------')
-        layout_superior.configure(bg = '#87CEFA')
+        layout_superior.configure(bg = 'red') #cor da label superior
         layout_superior.pack(side = TOP, expand = False, fill = 'x')
 
-        listagem_pokemon = Listbox() #div usada para alocar a lista dos nomes e id dos pokemons
-        listagem_pokemon.configure(relief = 'groove', border = 10, font = 'Times 12 bold') #configuraçõa de borda de fonte da div e os elemntos dentro dela
+        listagem_pokemon = Listbox(root, selectmode = SINGLE) #div usada para alocar a lista dos nomes e id dos pokemons
+        listagem_pokemon.configure(relief = 'groove', border = 5, font = 'Times 12 bold') #configuraçõa de borda de fonte da div e os elemntos dentro dela
         listagem_pokemon.pack(side = LEFT, expand = False, fill = 'both') #localidade, expansão onde a div foi colocada
+
+        def clicado(listagem_pokemon):
+            w = listagem_pokemon.widget
+            index = int(w.curselection()[0])
+            print(index) 
+
+           if index in pokemon_lista:
+               print('ok')
+
+            for index in pokemon_lista:
+                if index['ID'] == 0:
+                    print(TRUE)
+                    break
+                else:
+                    print(False)
+                    break
+            
+
+                
 
         div_1 = Scrollbar() #metedo de barra de rolagem
         div_1.pack(side = RIGHT,fill = 'y') #posição que foi inserido
         div_1.configure(command = listagem_pokemon.yview) #Ligado a div de listagem de pokemon
         listagem_pokemon.configure(yscrollcommand = div_1.set) #sentido da rolagem
+        listagem_pokemon.bind('<<ListboxSelect>>', clicado)
 
         for id_pokemon, pokemon in enumerate(pokemon_lista): #laço para gerar ordenar os nome e id dos pokemon
             texto = f'#{id_pokemon + 1:0>3}  -  {pokemon["Nome"]}' #variavel onde é armazenado o id e o nome dos pokemon pelo laço
             listagem_pokemon.insert(END,texto) #metodo que insere esses dados na div
+
 
         div_2 = Canvas(bg = "black") #div onde será inserido as imagens
         div_2.pack() 
@@ -28,6 +51,39 @@ class Application:
         div_2['height'] = 150 # altura
         img = PhotoImage(file = "mapa.png") 
         div_2.create_image(0,0, image = img ,anchor = NW)
+
+        
+        div_3 = Label(text = "Tipo : ")
+        div_3.configure(font = 'times 18 italic')
+        div_3.place(x = 173, y = 173)
+
+        tipo = Label(text = "------")
+        tipo.configure(font = 'times 12 bold')
+        tipo.place(x = 265, y = 178)
+
+        div_4 = Label(text = "Peso : ")
+        div_4.configure(font = 'times 18 italic')
+        div_4.place(x = 173, y = 200)
+
+        peso = Label(text = "------")
+        peso.configure(font = 'times 12 bold')
+        peso.place(x = 265, y = 200)
+
+        div_5 = Label(text = "Altura : ")
+        div_5.configure(font = 'times 18 italic')
+        div_5.place(x = 173, y = 227)
+
+        altura = Label(text = "------")
+        altura.configure(font = 'times 12 bold')
+        altura.place(x = 265, y = 230)
+
+        # button_teste = Button(text = "Ver mais", command = clicado)
+        # button_teste.pack(side = BOTTOM)
+
+      
+
+
+
 
 root.geometry("360x520+10+10")
 Application(root)
