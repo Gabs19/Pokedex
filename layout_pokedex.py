040ea2097c82c38ff2251ed2df84f7a5ddb1f842
gabs <gabriellima.glgs@gmail.com>
2019-10-21 20:43:03 -0300

    imagens e background inseridos

--- layout_pokedex.py
+++ layout_pokedex.py
@@ -13,8 +13,126 @@ class Application:
             pokemon = listagem_pokemon.widget
             index = int(pokemon.curselection()[0])
 
-            tipo_pokemon = f'{pokemon_lista[index]["Tipo"]}'
-            tipo["text"] = str(tipo_pokemon)
+
+            tipo_pokemon_1 = f'{pokemon_lista[index]["Tipo_1"]}'
+            tipo["text"] = str(tipo_pokemon_1)
+
+
+            if tipo_pokemon_1 == 'Fogo':
+                tipo["bg"] = '#ED6D12'
+            
+            elif tipo_pokemon_1 == 'Água':
+                tipo["bg"] = '#4578ED'
+               
+            elif tipo_pokemon_1 == 'Planta':
+                tipo["bg"] = '#69C23D'
+
+            elif tipo_pokemon_1 == 'Voador':
+                tipo["bg"] = '#8E6FEB'
+
+            elif tipo_pokemon_1 == 'Normal':
+                tipo["bg"] = '#9C9C63'
+            
+            elif tipo_pokemon_1 == 'Elétrico':
+                tipo["bg"] = '#F6C913'
+            
+            elif tipo_pokemon_1 == 'Terra':
+                tipo["bg"] = '#DBB54D'
+            
+            elif tipo_pokemon_1 == 'Aço':
+                tipo["bg"] = '#A0A0C0'
+            
+            elif tipo_pokemon_1 == 'Inseto':
+                tipo["bg"] = '#97A51D'
+            
+            elif tipo_pokemon_1 == 'Rocha':
+                tipo["bg"] = '#A48F32'
+
+            elif tipo_pokemon_1 == 'Gelo':
+                tipo["bg"] = '#7ECECE'
+
+            elif tipo_pokemon_1 == 'Lutador':
+                tipo["bg"] = '#AE2A24'
+            
+            elif tipo_pokemon_1 == 'Venenoso':
+                tipo["bg"] = '#923A92'
+
+            elif tipo_pokemon_1 == 'Noturno':
+                tipo["bg"] =  '#644E40'
+
+            elif tipo_pokemon_1 == 'Fantasma':
+                tipo["bg"] = '#655E88'
+            
+            elif tipo_pokemon_1 == 'Dragão':
+                tipo["bg"] = '#5E1DF7'
+
+            elif tipo_pokemon_1 == 'Psíquico':
+                tipo["bg"] = '#F73670'
+
+            elif tipo_pokemon_1 == 'Fada':
+                tipo["bg"] = '#e87890'
+
+            tipo_pokemon_2 = f'{pokemon_lista[index]["Tipo_2"]}'
+            tipo_secundario["text"] = str(tipo_pokemon_2)
+
+            if tipo_pokemon_2 == "":
+                tipo_secundario["bg"] = 'blue'
+
+            elif tipo_pokemon_2 != "":
+
+                if tipo_pokemon_2 == 'Fogo':
+                    tipo_secundario["bg"] = '#ED6D12'
+                
+                elif tipo_pokemon_2 == 'Água':
+                    tipo_secundario["bg"] = '#4578ED'
+                
+                elif tipo_pokemon_2 == 'Planta':
+                    tipo_secundario["bg"] = '#69C23D'
+
+                elif tipo_pokemon_2 == 'Voador':
+                    tipo_secundario["bg"] = '#8E6FEB'
+
+                elif tipo_pokemon_2 == 'Normal':
+                    tipo_secundario["bg"] = '#9C9C63'
+                
+                elif tipo_pokemon_2 == 'Elétrico':
+                    tipo_secundario["bg"] = '#F6C913'
+                
+                elif tipo_pokemon_2 == 'Terra':
+                    tipo_secundario["bg"] = '#DBB54D'
+                
+                elif tipo_pokemon_2 == 'Aço':
+                    tipo_secundario["bg"] = '#A0A0C0'
+                
+                elif tipo_pokemon_2 == 'Inseto':
+                    tipo_secundario["bg"] = '#97A51D'
+                
+                elif tipo_pokemon_2 == 'Rocha':
+                    tipo_secundario["bg"] = '#A48F32'
+
+                elif tipo_pokemon_2 == 'Gelo':
+                    tipo_secundario["bg"] = '#7ECECE'
+
+                elif tipo_pokemon_2 == 'Lutador':
+                    tipo_secundario["bg"] = '#AE2A24'
+                
+                elif tipo_pokemon_2 == 'Venenoso':
+                    tipo_secundario["bg"] = '#923A92'
+
+                elif tipo_pokemon_2 == 'Noturno':
+                    tipo_secundario["bg"] =  '#644E40'
+
+                elif tipo_pokemon_2 == 'Fantasma':
+                    tipo_secundario["bg"] = '#655E88'
+                
+                elif tipo_pokemon_2 == 'Dragão':
+                    tipo_secundario["bg"] = '#5E1DF7'
+
+                elif tipo_pokemon_2 == 'Psíquico':
+                    tipo_secundario["bg"] = '#F73670'
+
+                elif tipo_pokemon_2 == 'Fada':
+                    tipo_secundario["bg"] = '#e87890'
 
             altura_pokemon = f'{pokemon_lista[index]["Altura"]} m'
             altura["text"] = str(altura_pokemon)
@@ -44,40 +162,32 @@ class Application:
             texto = f'#{id_pokemon + 1:0>3}  -  {pokemon["Nome"]}' #variavel onde é armazenado o id e o nome dos pokemon pelo laço
             listagem_pokemon.insert(END,texto) #metodo que insere esses dados na div
 
-        div_3 = Label(text = 'Tipo:' )
-        div_3.configure(font = 'times 18 bold')
-        div_3.place(x = 270, y = 30)
-
-        tipo = Label(text = "??????/?????")
+        tipo = Label(text = "??????")
         tipo.configure(font = 'times 12 bold')
-        tipo.place(x = 340, y = 35)
+        tipo.place(x = 290, y = 35)
 
-        div_4 = Label(text = "Peso:")
-        div_4.configure(font = 'times 18 bold')
-        div_4.place(x = 270, y = 60)
+        tipo_secundario = Label(text = '??????')
+        tipo_secundario.configure(font = 'times 12 bold')
+        tipo_secundario.place(x = 370, y = 35)
 
-        peso = Label(text = "?????????")
+        peso = Label(text = "???? kg")
         peso.configure(font = 'times 12 bold')
-        peso.place(x = 340, y = 65)
+        peso.place(x = 290, y = 60)
 
-        div_5 = Label(text = "Altura:")
-        div_5.configure(font = 'times 18 bold')
-        div_5.place(x = 270, y = 90)
-
-        altura = Label(text = "???????")
+        altura = Label(text = "??? m")
         altura.configure(font = 'times 12 bold')
-        altura.place(x = 370, y = 95)
+        altura.place(x = 290, y = 90)
         
-        load = Image.open("mapa.png")
+        load = Image.open("noid.png")
         render = ImageTk.PhotoImage(load) 
         img = Label(image = render)
         img.image = render
-        img.place(x = 174, y = 30)
+        img.place(x = 180, y = 30)
         img["width"] = 80
         img["height"] = 80
 
         img = Label(image = "")
-        img.place(x = 174 , y = 30)
+        img.place(x = 180 , y = 30)
 
         div_6 = Label(text = "---- Dados -----")
         div_6.configure(bg = 'red')
